3f35a90c3be6983989bc75624f7d3501164132a0
JaveedBeargrylls <76989549+JaveedBeargrylls@users.noreply.github.com>
2021-07-03 18:57:30 +0530

    Modified to get the UserDetails UC-1

--- Address-Book/src/com/bridgelabz/AddressBook/AddressBook.java
+++ Address-Book/src/com/bridgelabz/AddressBook/AddressBook.java
@@ -3,9 +3,16 @@ package com.bridgelabz.AddressBook;
 public class AddressBook {
 	
 	// created a AddressBook
-	public void main(String[] args)
+	public static void main(String[] args)
 	{
 		System.out.println("Welcome to the Address Book");
+		
+		// Created the AddressBook
+		
+		UserDetails obj = new UserDetails("Javeed","Dudekula","BoundStreet","Chittoor","517002","Andhra","9100188627","javeed12345@gmail.com");
+		System.out.print("\n"+"\t ADDRESS BOOK"+"\n NAME = "+obj.getFirstName()+obj.getLastName());
+        System.out.print("\n ADDRESS = "+obj.getAddress()+", "+obj.getCity()+", "+obj.getZip());
+        System.out.println("\n PhoneNUmber = "+obj.getMobileNo()+"\n Email = "+obj.getEmail() );
 	
 	}
 }
