cfd53a4d7a6b5444f198d2d29845b6c9ee525826
JaveedBeargrylls <76989549+JaveedBeargrylls@users.noreply.github.com>
2021-07-03 18:21:05 +0530

    Added the AddressBook With Welcome Note

---
+++ Address-Book/src/com/bridgelabz/AddressBook/AddressBook.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.AddressBook;
+
+public class AddressBook {
+	
+	// created a AddressBook
+	public void main(String[] args)
+	{
+		System.out.println("Welcome to the Address Book");
+	
+	}
+}
