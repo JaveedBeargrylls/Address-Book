db739b754b91519bb2dc2af56dfcb287ea73c58b
JaveedBeargrylls <76989549+JaveedBeargrylls@users.noreply.github.com>
2021-07-05 12:21:46 +0530

    UC-3 Updated to Edit details

--- Address-Book/src/com/bridgelabz/AddressBook/AddressBook.java
+++ Address-Book/src/com/bridgelabz/AddressBook/AddressBook.java
@@ -7,47 +7,14 @@ public class AddressBook {
 	// created a AddressBook
 	public static void main(String[] args) 
 	{
+		System.out.println("Welcome to the address book");
+		
 		Scanner sc = new Scanner(System.in);
-		String[] arr = new String[14]; 
-		int i;
-		String Fname, Lname, Addr, city, state, email, phone, zip; 
-		UserDetails obj = new UserDetails("Javeed", "D","AbcStreet", "Chittoor", "Andhra", "517001","9100188627", "javeed12345@gmail.com");
-		Fname=obj.getFirstName();
-		Lname=obj.getLastName();
-		Addr=obj.getAddress();
-		city=obj.getCity();
-		state=obj.getState();
-		email=obj.getEmail();
-		phone=obj.getMobileNo();
-		zip=obj.getZip();
+
+		System.out.println("1. Add Details 2. Update Details");
+
+		int choice = sc.nextInt();
 		
-		arr[0] = Fname;
-		arr[1] = Lname;
-		arr[2] = Addr;
-		arr[3] = city;
-		arr[4] = state;
-		arr[5] = email;
-		arr[6] = phone;
-		arr[7] = zip;
-		
-		// Stored in array form
-		
-		System.out.println("Person data in array:");
-		
-		
-		
-		// added the new contact ot the address book
-		
-		for ( i =7; i<14; i++)
-		{
-			arr[i] = sc.next();
-		}
-		
-		System.out.println("UserDetails");
-		
-		for ( i = 0; i < arr.length; i++) 
-		{
-			System.out.println(arr[i]);
-		}
+		sc.close();
 	}		
 }
