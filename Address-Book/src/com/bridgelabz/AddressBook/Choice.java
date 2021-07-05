975083d9bd6d7d8a1666104e9b1bffb354e6a429
JaveedBeargrylls <76989549+JaveedBeargrylls@users.noreply.github.com>
2021-07-05 12:22:24 +0530

    Updated to Edit Details UC-3

---
+++ Address-Book/src/com/bridgelabz/AddressBook/Choice.java
@@ -0,0 +1,112 @@
+package com.bridgelabz.AddressBook;
+
+import java.util.Scanner;
+
+public class Choice {
+	static Scanner sc = new Scanner(System.in);
+	
+	static UserDetails arr[] = new UserDetails[10];
+	static int count = 0;
+	
+		String firstName;
+		String lastName;
+		String address;
+		String city;
+		String state;
+		String zip;
+		String mobileNo;
+		String email;
+		
+	public Integer choice(int choice) {
+		
+		while(true) {
+			switch(choice) {
+			case 1: System.out.println("Add Details");
+			System.out.println("Enter how many contacts to be added to the AddressBook");
+			int num=sc.nextInt();
+			
+			for(int i = 0; i < num; i++)
+			{
+				System.out.println("Enter first name");
+				firstName = sc.next();
+
+				System.out.println("Enter last name");
+				lastName = sc.next();
+				
+				System.out.println("Enter address");
+				address = sc.next();
+				
+				System.out.println("Enter city");
+				city = sc.next();
+				
+				System.out.println("Enter State");
+				state = sc.next();
+				
+				System.out.println("Enter zip");
+				zip = sc.next();
+				
+				System.out.println("Enter mobileNo");
+				mobileNo = sc.next();
+				
+				System.out.println("Enter Email Id");
+				email = sc.next();
+				
+				arr[count] = new UserDetails(firstName, lastName, address, city, state, zip, mobileNo, email);
+				count++;
+				break;
+				}
+			System.out.println("Details added Successfully");
+			break;
+			 
+			case 2: System.out.println("Update Details");
+			
+			System.out.println("enter first name to Edit that Contact");
+			
+			firstName = sc.next();
+			
+			for(int i = 0; i < count; i++)
+			{
+				if(arr[i].getFirstName() == firstName && arr[i] != null) 
+				{
+					
+				System.out.println("Enter first name");
+				firstName = sc.next();
+				arr[i].setFirstName(firstName);
+
+				System.out.println("Enter last name");
+				lastName = sc.next();
+				arr[i].setLastName(lastName);
+				
+				System.out.println("Enter address");
+				address = sc.next();
+				arr[i].setAddress(address);
+				
+				System.out.println("Enter city");
+				city = sc.next();
+				arr[i].setCity(city);
+				
+				System.out.println("Enter State");
+				state = sc.next();
+				arr[i].setState(state);
+				
+				System.out.println("Enter zip");
+				zip = sc.next();
+				arr[i].setZip(zip);
+				
+				System.out.println("Enter mobileNo");
+				mobileNo = sc.next();
+				arr[i].setMobileNo(mobileNo);
+				
+				System.out.println("Enter Email Id");
+				email = sc.next();
+				arr[i].setEmail(email);
+				break;
+
+				}				
+			}
+			System.out.println("Details Updated");
+			break;
+			}	
+		}
+	}
+}
